878e2c4ece048203bd83e64d964c44b16227d69b
Pablo <amepablo@gmail.com>
2022-06-21 17:17:50 -0300

    clase 1: ¿Qué es el scope? Global Scope

---
+++ src/scope/global.js
@@ -0,0 +1,25 @@
+// Variables
+
+var a;          // declarando
+var b = b;      // declaramos / asignamos
+b = 'bb';       // reasignación
+var a = 'aa'    // redeclaración
+
+// Global Scope
+var fruit = 'Apple';    // global
+console.log(fruit);
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+
+function countries() {
+    countries = 'Argentina';    // global
+    console.log(countries);
+}
+
+countries();
+console.log(countries);
+
