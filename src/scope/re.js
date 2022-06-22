311e9176500e6787e4aa6c4877be9fcfed06034a
Pablo <amepablo@gmail.com>
2022-06-22 12:30:20 -0300

    clase 5: Reasignación y redeclaración

---
+++ src/scope/re.js
@@ -0,0 +1,26 @@
+var firstName;  // Underfined       // Declaramos
+firstName = 'Pablo';                // Asignamos
+console.log(firstName);             // Mostramos
+
+
+var lastName = 'Daniel';            // declarar / asignar
+lastName = 'Amé';                   // reasignar
+console.log(lastName);
+
+var secondName = 'Jorge';           // declarar / Redesignar
+var secondName = 'Luis';            // reasignado
+console.log(secondName);
+
+// Let
+let fruit = 'apple' // declarar y asignar
+fruit = 'Kiwi';     // reasignar
+console.log(fruit);
+// let fruit = 'Banana'    // Cannot redeclare block-scoped variable 'fruit'
+
+// const
+const animal = 'dog';   // declara y asigna
+// animal = 'cat';      // reasignar
+console.log(animal);    // Assignment to constant variable
+
+const vehicles = [];     // inicializar array
+vehicles.push('fitito');
