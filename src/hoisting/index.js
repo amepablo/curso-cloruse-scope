299b10ac8c493bfc3bb8f67afb39213fb6735999
Pablo <amepablo@gmail.com>
2022-06-22 15:42:04 -0300

    clase 9: Que es el hoisting?

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+// console.log(nameOfDog);         // nameOfDog = underfined
+// var nameOfDog = 'Snoopy';       
+// console.log(nameOfDog);         // nameOfDog = snoopy
+
+nameOfDog();            // Devuelve underfined
+
+function nameOfDog() {      // Se eleva
+    console.log(`El mejor perrito es ${snoopy}`)
+}
+
+var snoopy = 'snoopy';      // Se eleva
